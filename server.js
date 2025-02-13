834b4eeeba79811cbe41a9cf8f7eaafce3b0d81c
ABDULLAH-ALTHAF <mbros123.althaf@gmail.com>
2025-02-13 13:49:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb://127.0.0.1:27017/privateChat", {
+mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
